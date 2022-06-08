fbd234469952b80d391dba021fd82c43a155b4e6
jeffp27 <jrpowel9@ncsu.edu>
2022-06-08 15:06:09 -0400

    IT WORKS :D

--- celllists.cpp
+++ celllists.cpp
@@ -40,7 +40,8 @@ const double Na = 6.022 * std::pow(10, 23); // Atoms per mole
 const int numTimeSteps = 10000; // Parameters to change for simulation
 const double dt_star= .001;
 
-const int N = 108000 SIGMA = 3.405; // Angstroms
+const int N = 4000;
+const int SIGMA = 3.405; // Angstroms
 const double EPSILON = 1.6540 * std::pow(10, -21); // Joules
 const double EPS_STAR = EPSILON / Kb; // ~ 119.8 K
 
