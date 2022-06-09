41ccea9c9debac8974ddba444893f80ed3687f6b
Jeff-Pow <jeff@pop-os.localdomain>
2022-06-09 09:44:57 -0400

    .

--- bs_Slices.cpp
+++ bs_Slices.cpp
@@ -342,6 +342,7 @@ double calcForces(std::vector<Atom>& atomList, std::ofstream& debug) { // Cell p
     for (int i = 0; i < numCellsPerDirection; i++) {
         addedPotentials[i] = pool.submit(calcForcesForSlice, i, std::ref(atomList)); 
     }
+    pool.wait_for_tasks();
     for (int i = 0; i < numCellsPerDirection; i++) {
         netPotential += addedPotentials[i].get();
     }
